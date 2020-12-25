a2a0873cb9406ccf0037907c1922bf34614dbd41
edmundo papi <edmundopierre@yahoo.com>
2020-12-25 15:23:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,18 +1,58 @@
-console.log('%c HI', 'color: firebrick')
-const imgUrl= "https://dog.ceo/api/breeds/image/random/4";
-fetch('imgUrl')
-.then(response =>response.json())
-.then(result =>{
-  dogImage(result.message)
+// console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded',(event)=>{
+ loadImages();
+ loadBreeds();
 })
 
-function dogImages(dogs) {
-  console.log(dogs)
-  dogs.forEach(dog =>{
-    console.log(dog)
-    const goodBoyContainer = document.querySelector(#dog-image-contenair)
-    const goodBoy = document.createElement('img')
-    goodBoy.src = dog
-    goodBoyContainer.appendChild(goodBoy)
-  });
+function loadImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  return fetch(imgUrl).then(res =>res.json())
+  .then(json=>json.message.forEach(
+  y=>addImage(y) ))
 }
+
+function addImage(image) {
+  const contenair = document.getElementById("dog-image-container")
+  const im = document.createElement('img')
+  im.src = image
+  contenair.appendChild(im)
+}
+function  loadBreeds() {
+
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+ return fetch(breedUrl).then(res=>res.json()).then(json=>breeds(json.message))
+}
+
+function breeds(json) {
+  let u = document.getElementById("dog-breeds")
+
+  for (const k in json) {
+    let i = document.createElement('li')
+    i.innerText = `${k}`;
+     u.appendChild(i);
+     i.addEventListener('click', (event)=>{
+       event.target.style.color ='blue';
+     })
+  }
+
+}
+
+
+
+// const selected = ducument.getElementById('breed-dropdown');
+// const value = selected.value;
+//    for (const k in json) {
+//      filter(k=> {
+//         k.startsWith(value)
+//         let i = document.querySelector('label')
+//         let p = document.createElement('p')
+//         p.innerText = `${k}`;
+//         i.appendChild(p);
+//
+//      })
+//
+//
+//
+// }
+// }
+//
